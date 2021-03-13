601bd36dc3ef8c7edfe17b193c2c31aa7b497e74
awerlogus <awerlogus@yandex.ru>
2021-03-13 17:56:49 +0300

    Added json data type

---
+++ packages/data-types/lib/json.d.ts
@@ -0,0 +1,20 @@
+import { Option } from './option'
+
+// SECTION Types
+
+/** Represents json array */
+export type JsonArray = ReadonlyArray<Json>
+
+/** Represents json object */
+export type JsonRecord = { readonly [key: string]: Json }
+
+/** Represents all possible json values */
+export type Json = boolean | number | string | null | JsonRecord | JsonArray | readonly []
+
+// SECTION Library
+
+/** Encodes json value to string */
+export const encode: (data: Json) => string
+
+/** Decodes json value from string */
+export const decode: (data: string) => Option<Json>
